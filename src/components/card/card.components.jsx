3312f496eb18cfe9f5cbaf108ecad5eda2830c3b
Doddy Amijaya <doddyillona@gmail.com>
2020-11-01 17:34:32 -0600

    change some css

--- src/components/card/card.components.jsx
+++ src/components/card/card.components.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import './card.styles.css'
 export const Card = (props) => (
     <div className ='card-container'>
-    <img alt="people" src = {`https://robohash.org/${props.people.id}?set=2%size=180x180`} />
+    <img alt="people" src = {`https://robohash.org/${props.people.id}?set=1%size=180x180`} />
         <h2>{props.people.name}</h2>
         <p>{props.people.email}</p>
+        <p>phone # {props.people.phone}</p>
+        <p>Address</p>
+        <p>{props.people.address.street}, {props.people.address.suite}</p>
+        <p>{props.people.address.city}, {props.people.address.state} {props.people.address.zipcode}</p>
+        <p></p>
+        <p></p>
     </div>
 );
